79e8c2c4138f562a6929f133fac756b40d6ca81b
heartlesss <196399488+reyzxz@users.noreply.github.com>
2025-09-17 08:56:40 +0700

    Create model.rs

---
+++ src/model.rs
@@ -0,0 +1,9 @@
+pub fn load_model(path: &str) {
+    // TODO: implement model loading
+    println!("Loading model from {}", path);
+}
+
+pub fn hash_model() {
+    // TODO: implement model hashing
+    println!("Hashing model...");
+}
